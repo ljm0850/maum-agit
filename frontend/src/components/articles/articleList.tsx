99edcb636f21c25759672363d7ac68570ecc69dd
ljm0850 <dlwoals0850@gmail.com>
2025-09-02 16:46:21 +0900

    [maum-agit] bugfix: 글이 없을 때 새 글 모달 안뜨던 문제 해결

--- frontend/src/components/articles/articleList.tsx
+++ frontend/src/components/articles/articleList.tsx
@@ -91,10 +91,16 @@ export default function ArticleList(){
   // 게시글이 없을 때
   if (!articles || articles.length === 0){
     return (
+      <div>
       <Container>
         <h1> 게시글이 없습니다.</h1>
         <Button variant="outline-info" onClick={handleOpenCreateModal}>새 글 작성</Button>
       </Container>
+      <ArticleFormModal
+        isOpen={isFormModalOpen} 
+        onClose={handleCloseFormModal} 
+      />
+      </div>
     )
   }
 
